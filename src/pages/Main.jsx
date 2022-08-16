5b1532a6d37e309da5a880e69d26fe37256564b5
Lika <lika.rossinskaya@gmal.com>
2022-08-17 00:42:00 +0300

    input

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux';
 import { getUserEmail } from 'redux/auth/auth-selectors';
+import BalancePage from './Balance/BalancePage';
+
 export const Main = () => {
   const emailToShow = useSelector(getUserEmail);
   console.log(emailToShow);
   return (
     <div>
       <h2>Hello {emailToShow}! </h2>
+      <BalancePage />
     </div>
   );
 };
