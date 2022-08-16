08197e821903a99987a334344febad387b296367
Iryna <pysarenkoiryna@gmail.com>
2022-08-16 18:55:28 +0300

    update

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -7,12 +7,12 @@ export const MainContainer = () => {
   return (
     <div className={styles.Container}>
       <div className={styles.Fon}>
-        <img src={Rectangle} alt="Fon" />
+        <img src={Rectangle} alt="Fon" width="100%" />
         <div className={styles.FonNuts}>
-          <img src={NutsFon} alt="" />
+          <img src={NutsFon} alt="" width="100%" />
         </div>
         <div className={styles.LogoKapusta}>
-          <img src={LogoKapusta} alt="" />
+          <img src={LogoKapusta} alt="" width="100%" />
         </div>
       </div>
       <div className={styles.Nutsmini}>
