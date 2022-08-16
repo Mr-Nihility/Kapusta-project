b5ba8d98ef1611985fc92d81d1260d053b9fd029
Iryna <pysarenkoiryna@gmail.com>
2022-08-16 19:35:19 +0300

    update

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -3,11 +3,28 @@ import Rectangle from './images/Rectangle.png';
 import NutsFon from './images/NutsFon.png';
 import Nutsmini from './images/Nutsmini.png';
 import LogoKapusta from './images/LogoKapusta.png';
+import { useSelector } from 'react-redux';
+// import { getToken } from 'redux/auth/auth-selectors';
 export const MainContainer = () => {
-  return (
+  const inAuth = useSelector(state => state.auth.accessToken);
+  console.log(inAuth);
+  return inAuth ? (
+    <>
+      <div className={styles.Container}>
+        <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
+        <img
+          className={styles.FonNutsInAuth}
+          src={NutsFon}
+          alt=""
+          width="100%"
+        />
+      </div>
+    </>
+  ) : (
     <div className={styles.Container}>
       <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
       <img className={styles.FonNuts} src={NutsFon} alt="" width="100%" />
+
       <img
         className={styles.LogoKapusta}
         src={LogoKapusta}
