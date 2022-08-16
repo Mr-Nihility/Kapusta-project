47fcca499de0a21ff7c08693768b515a72dc6e57
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-08-16 16:25:14 +0300

    save

--- src/pages/Statisticts/Statisticts.jsx
+++ src/pages/Statisticts/Statisticts.jsx
@@ -5,7 +5,12 @@ export const Statisticts = () => {
 		<>
 			<Link to='/' className={Style.button_goHome}>Main page</Link>
 			<div className={Style.infoForUserBalance}>
-				<p className={Style.textBalance}></p>
+				<p className={Style.textBalance}>Balance: <span className={Style.span_balance}>'{}'</span></p>
+				<button className={Style.button_confirm}>CONFIRM</button>
+			</div>
+			<div className={Style.month_switch}>
+				<p className={Style.month_switchText}>Current period:</p>
+				<p className={Style.month_switchTextBig}><span className={Style.month_switch}></span>November 2019<span className={Style.month_switch}></span></p>
 			</div>
 		</>
 	)
