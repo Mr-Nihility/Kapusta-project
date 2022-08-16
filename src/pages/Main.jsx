f456d28d18f532ce8ef327d87a64105e295c13e2
Lika <lika.rossinskaya@gmal.com>
2022-08-17 00:55:04 +0300

    main corrected

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,8 @@
-import { useSelector } from 'react-redux';
-import { getUserEmail } from 'redux/auth/auth-selectors';
 import BalancePage from './Balance/BalancePage';
-
-// import { useSelector } from 'react-redux';
-// import { getUserEmail } from 'redux/auth/auth-selectors';
 
 import { TransactionForm } from 'components/TransactionForm/TransactionForm';
 
-// import { Statisticts } from './Statisticts/Statisticts'{/* <Statisticts /> */}
-
 export const Main = () => {
-  // const emailToShow = useSelector(getUserEmail);
-
   return (
     <div>
       <h2>Hello {emailToShow}! </h2>
