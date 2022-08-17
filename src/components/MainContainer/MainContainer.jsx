2013341a882b0a344731f11b9828c582bee4997d
Iryna <pysarenkoiryna@gmail.com>
2022-08-17 13:14:53 +0300

    update

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -10,22 +10,22 @@ export const MainContainer = ({ children }) => {
   console.log(inAuth);
   return inAuth ? (
     <>
-      <div className={styles.Container}>
+      <div className={styles.Container2}>
         {children}
-        <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
+        {/* <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
         <img
           className={styles.FonNutsInAuth}
           src={NutsFon}
           alt=""
           width="100%"
-        />
+        /> */}
       </div>
     </>
   ) : (
-    <div className={styles.Container}>
+    <div className={styles.Container1}>
       {children}
-      <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
-      <img className={styles.FonNuts} src={NutsFon} alt="" width="100%" />
+      {/* <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
+      <img className={styles.FonNuts} src={NutsFon} alt="" width="100%" /> */}
 
       <img
         className={styles.LogoKapusta}
@@ -34,7 +34,7 @@ export const MainContainer = ({ children }) => {
         width="100%"
       />
 
-      <img src={Nutsmini} alt="" className={styles.Nutsmini} />
+      {/* <img src={Nutsmini} alt="" className={styles.Nutsmini} /> */}
     </div>
   );
 };
