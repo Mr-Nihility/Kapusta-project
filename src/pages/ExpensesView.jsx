052e1782e3e65468dc9c7bda2925769435dd435f
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-08-17 21:13:46 +0300

    upf

--- src/pages/ExpensesView.jsx
+++ src/pages/ExpensesView.jsx
@@ -1,23 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Tablelist from 'components/TableList/TableList';
 import { TransactionForm } from 'components/TransactionForm/TransactionForm';
+import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
-import { addExpanses } from 'redux/transaction/transaction-operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { getExpansesCategories } from 'redux/categories/catrgories-operation';
+import { addExpanses, getExpanses } from 'redux/transaction/transaction-operations';
+import { getExpence } from 'redux/transaction/transactions-selectors';
 
 import BalancePage from './Balance/BalancePage';
 export default function ExpensesView() {
-  const rCategory = [
-    'Транспорт',
-    'Продукты',
-    'Здоровье',
-    'Развлечения',
-    'Всё для дома',
-    'Техника',
-    'Коммуналка и связь',
-    'Спорт и хобби',
-    'Образование',
-    'Прочее',
-  ];
+  // const rCategory = [
+  //   'Транспорт',
+  //   'Продукты',
+  //   'Здоровье',
+  //   'Развлечения',
+  //   'Всё для дома',
+  //   'Техника',
+  //   'Коммуналка и связь',
+  //   'Спорт и хобби',
+  //   'Образование',
+  //   'Прочее',
+  // ];
   const engCategory = [
     'Transport',
     'Products',
@@ -32,10 +36,18 @@ export default function ExpensesView() {
   ];
 
   const dispatch = useDispatch();
+  
+  const rCategory = useSelector(getExpansesCategories);
+  useEffect(()=>{
+    dispatch(getExpanses())
+    dispatch(getExpansesCategories())
+  },[]) 
+console.log(rCategory);
 
   const onSubmit = data => {
     
     dispatch(addExpanses(data));
+    
   };
 
   return (
