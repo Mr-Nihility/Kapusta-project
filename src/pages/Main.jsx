c50d2325f7e4cd4ab001b5a94f9ef0f4265a6de5
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-08-17 10:54:00 +0300

    save

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux';
 import { getUserEmail } from 'redux/auth/auth-selectors';
-import { Statisticts } from './Statisticts/Statisticts'
+// import { Statisticts } from './Statisticts/Statisticts'	//<Statisticts />
 export const Main = () => {
   const emailToShow = useSelector(getUserEmail);
   console.log(emailToShow);
   return (
     <div>
       <h2>Hello {emailToShow}! </h2>
-			<Statisticts />
+		
     </div>
   );
 };
