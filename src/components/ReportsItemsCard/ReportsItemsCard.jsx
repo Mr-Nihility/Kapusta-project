a7470ae819bfb9be4a457f6a9b2a50c2ab86ea5e
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-08-18 19:43:42 +0300

    save

--- src/components/ReportsItemsCard/ReportsItemsCard.jsx
+++ src/components/ReportsItemsCard/ReportsItemsCard.jsx
@@ -1,8 +1,19 @@
 import Style from './ReportsItemsCard.module.css';
 import { Icons } from '../Icons/Icons';
+import { useState } from 'react';
+
 export const ReportsItemsCard = ({total, category, id}) => {
-  return (
-      <li className={Style.item_spend} key={id}>
+	const [idEl, setIdEl] =useState('');
+	const [categoryEl, setCategoryEl] =useState('');
+	
+  const handelClickOnCategory = (id, category ) => {
+		console.log(id, category);
+		setIdEl(id);
+		setCategoryEl(category);
+	}
+	
+	return (
+      <li className={Style.item_spend} key={id} onClick={() =>handelClickOnCategory(id, category)}>
         <p className={Style.item_text}>{total + '.00'}</p> 
           <Icons 
 					  name={category}
