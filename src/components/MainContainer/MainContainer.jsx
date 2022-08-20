58f22ca4762d3775128cb3d4bf1aa50217664566
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-08-20 13:55:05 +0300

    commit 1

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -3,11 +3,13 @@ import styles from './MainContainer.module.css';
 import LogoKapusta from './images/LogoKapusta.png';
 import { useSelector } from 'react-redux';
 import { getSuccessToken } from 'redux/auth/auth-selectors';
+import Nutsmini from './images/Nutsmini.png';
 export const MainContainer = ({ children }) => {
   const hasToken = useSelector(getSuccessToken);
   return hasToken ? (
     <>
       <div className={styles.Container2}>
+        <div className={styles.bgColor2}></div>
         {children}
         {/* <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
         <img
@@ -19,19 +21,21 @@ export const MainContainer = ({ children }) => {
       </div>
     </>
   ) : (
-    <div className={styles.Container1}>
-      {children}
-      {/* <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
-      <img className={styles.FonNuts} src={NutsFon} alt="" width="100%" /> */}
+    <>
+      <div className={styles.bgColor}></div>
+      <div className={styles.innerWrapperForm}>
+        <img
+          className={styles.LogoKapusta}
+          src={LogoKapusta}
+          alt=""
+          width="100%"
+        />
+        {children}
+        {/* <img className={styles.Fon} src={Rectangle} alt="Fon" width="100%" />
+          <img className={styles.FonNuts} src={NutsFon} alt="" width="100%" /> */}
+      </div>
 
-      <img
-        className={styles.LogoKapusta}
-        src={LogoKapusta}
-        alt=""
-        width="100%"
-      />
-
-      {/* <img src={Nutsmini} alt="" className={styles.Nutsmini} /> */}
-    </div>
+      <img src={Nutsmini} alt="" className={styles.Nutsmini} />
+    </>
   );
 };
